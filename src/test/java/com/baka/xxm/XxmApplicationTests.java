d9871a56c02d07ec5467e88226f12207d2961bdd
dsxxm <2531222159@qq.com>
2026-06-19 12:29:54 +0800

    init
    init project

---
+++ src/test/java/com/baka/xxm/XxmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baka.xxm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
